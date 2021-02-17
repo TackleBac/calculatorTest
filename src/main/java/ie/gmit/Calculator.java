d788ddbd6c035161c39a07a3027e22fbd2a5b803
Tacklbac <small.rick.y@gmail.com>
2021-02-17 23:29:03 +0000

    Added more methods and tests to confirm they work

--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -7,24 +7,25 @@ package ie.gmit;
 
 public class Calculator {
 
-	private int firstNum;
-	private int secondNum;
-	private int result;
-
-	public Calculator() {
-		this.firstNum = 0;
-		this.secondNum = 0;
-	}
-
-
-	public Calculator(int firstNum, int secondNum) {
-		this.firstNum = firstNum;
-		this.secondNum = secondNum;
-	}
-
 	// This method will add two numbers
 	public int add(int numA, int numB){
-		result = numA + numB;
+		int result = numA + numB;
 		return result;
 	}
+
+	public int sub(int numA, int numB){
+		int result = numA - numB;
+		return result;
+	}
+
+	public double multiply(int numA, int numB){
+		int result = numA * numB;
+		return result;
+	}
+
+	public double divide(int numA, int numB){
+		int result = numA / numB;
+		return result;
+	}
+
 }
